e4f0ba7e22b2d872402a6da9875898c506c2b16f
Shivank Mittal <shivankmittal@shivanks-laptop.home>
2024-06-15 16:16:00 +0200

    finished Welcome section with dynamic css function and media query

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { MainComponent } from './pages/main/main.component';
+import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, MainComponent],
+  imports: [RouterOutlet, FooterComponent, MainComponent, WelcomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/menu-card/menu-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu-card.component.html',
+  styleUrl: './menu-card.component.scss'
+})
+export class MenuCardComponent {
+
+}
